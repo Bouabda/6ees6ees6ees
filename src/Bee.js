39850b5a32a1122cbeb5342b5826b62f13c5da00
Bouabda <abdaoui.wala@gmail.com>
2020-03-20 21:56:02 +0100

    6ees done

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor(age,color,food){ 
+  super(age, color, food);
+  
+  this.age = age || 5;
+  this.color = color || "yellow";
+  this.job = "Keep on growing"; 
+  }
+
 };
+
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+  constructor (age,color,food){
+    super(age,color,food);
+    
+    this.age = age || 40;
+    this.job = "gamble";
+    this.color = color||"grey"
+    this.canFly = false;
+    this.treasureChest = [];
+
+   }
+   forage() { 
+      return "I am too old, let me play cards instead";
+   }
+   gamble(treasure) { 
+    super.forage(treasure);
+ }
 };
